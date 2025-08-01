881b736b99ee5a6a45c060c77ee2e646f769d0ea
Daniel Santos Parron <danielsaparron@gmail.com>
2025-08-01 20:28:40 -0300

    Hello World no Javascript

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ let resultado = somar(5, 3);
 
 // Exibindo o resultado no console
 console.log("O resultado da soma é:", resultado);
+
+// Função para exibir a mensagem no console
+function mostrarMensagem() {
+    console.log("Você clicou no botão!");
+}
+
+// Chamando a função para testar
+mostrarMensagem();
