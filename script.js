8bc25f09348fb872f1fa58919b36b71417d3f8fb
Daniel Santos Parron <danielsaparron@gmail.com>
2025-08-01 20:27:48 -0300

    Soma no Javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+// Função que soma dois números
+function somar(a, b) {
+    return a + b;
+}
+
+// Chamando a função e armazenando o resultado
+let resultado = somar(5, 3);
+
+// Exibindo o resultado no console
+console.log("O resultado da soma é:", resultado);
